4529aff092e02486cf91a0c825a6f928a16bf817
lucar <lucar@Cortana>
2020-10-07 16:54:04 -0700

    Commented code and created functioning methods to be used and implemented

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -1,4 +1,8 @@
-
+/*	Name: Luca Robins
+ * 	Date: 10/7/2020
+ * 	Class: CSE360
+ * 	Assignment: Create an addingmachine program based off thge given template.
+ */
 
 
 
@@ -6,26 +10,47 @@
 package cse360assignment02;
 
 public class AddingMachine {
-  private int total;
-  
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
-  public int getTotal () {
-    return 0;
-  }
-  
-  public void add (int value) {
-  }
 
-  public void subtract (int value) {
-  }
+	// Instantiate variables used throughout program.
+	private int total;
+	private String history;
 
-  public String toString () {
-    return "";
-  }
+	public AddingMachine() {
+		//Instantiate running total and history of activity to 0.
+		total = 0;
+		history = "0";
+	}
 
-  public void clear() {
-  }
+	//Used to return the current total.
+	public int getTotal() {
+		return total;
+	}
+
+	//Used to add the given int to the total. Updates the total and adds the action to the history string.
+	public void add(int value) {
+		total = total + value;
+		String addHist = (" + " + value);
+		history = history + addHist;
+		;
+
+	}
+
+	//Used to subtract the given int from the total. Updates the total and adds the action to the history string.
+	public void subtract(int value) {
+		total = total - value;
+		String subHist = (" - " + value);
+		history = history + subHist;
+		;
+	}
+
+	//Returns the entire history of actions that were done to the total. 
+	public String toString() {
+		return (history);
+	}
+
+	//Rests the total and history string to 0, effectively giving it a clean slate and deleting all previous actions.
+	public void clear() {
+		this.history = "0";
+		this.total = 0;
+	}
 }
